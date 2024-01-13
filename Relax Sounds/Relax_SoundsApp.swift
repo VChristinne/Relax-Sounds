032bc85cec839a4c9313debe00d69d53be2694c0
Christinne <vanessa0christinne@gmail.com>
2024-01-13 22:21:32 +0100

    Initial Commit

---
+++ Relax Sounds/Relax_SoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Relax_SoundsApp.swift
+//  Relax Sounds
+//
+//  Created by Christinne on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Relax_SoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
